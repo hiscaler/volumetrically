1b46d7f8bba41bb0cd43a85ea09fb21fd9ee14e9
hiscaler <hiscaler@gmail.com>
2025-11-22 15:06:15 +0800

    Chg: 移除没必要暴露的 decimal.Decimal

--- weight.go
+++ weight.go
@@ -3,20 +3,20 @@ package volumetrically
 import "github.com/shopspring/decimal"
 
 type Weight struct {
-	decimal.Decimal
+	value decimal.Decimal
 }
 
 // Gram 返回克，precision表示小数点位数
 func (w Weight) Gram(precision int32) float64 {
-	return w.Round(precision).InexactFloat64()
+	return w.value.Round(precision).InexactFloat64()
 }
 
 // Kilogram 返回千克，precision表示小数点位数
 func (w Weight) Kilogram(precision int32) float64 {
-	return w.Div(decimal.NewFromInt(1000)).Round(precision).InexactFloat64()
+	return w.value.Div(decimal.NewFromInt(1000)).Round(precision).InexactFloat64()
 }
 
 // Pound 磅，precision表示小数点位数
 func (w Weight) Pound(precision int32) float64 {
-	return w.Div(decimal.NewFromFloat(453.59237)).Round(precision).InexactFloat64()
+	return w.value.Div(decimal.NewFromFloat(453.59237)).Round(precision).InexactFloat64()
 }
--- weight_test.go
+++ weight_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestWeight_Gram(t *testing.T) {
-	w := Weight{Decimal: decimal.NewFromFloat(123.456)}
+	w := Weight{value: decimal.NewFromFloat(123.456)}
 	expected := 123.46
 	if gram := w.Gram(2); gram != expected {
 		t.Errorf("期望值是 %.2f g，但得到的是 %.2f g", expected, gram)
@@ -15,7 +15,7 @@ func TestWeight_Gram(t *testing.T) {
 }
 
 func TestWeight_Kilogram(t *testing.T) {
-	w := Weight{Decimal: decimal.NewFromFloat(123456)}
+	w := Weight{value: decimal.NewFromFloat(123456)}
 	expected := 123.46
 	if kg := w.Kilogram(2); kg != expected {
 		t.Errorf("期望值是 %.2f kg，但得到的是 %.2f kg", expected, kg)
@@ -24,7 +24,7 @@ func TestWeight_Kilogram(t *testing.T) {
 
 func TestWeight_Pound(t *testing.T) {
 	// 1000克约等于 2.20462 磅
-	w := Weight{Decimal: decimal.NewFromInt(1000)}
+	w := Weight{value: decimal.NewFromInt(1000)}
 	// 内部转换是除以 453.59237
 	// 1000 / 453.59237 = 2.20462262...
 	expected := 2.20
